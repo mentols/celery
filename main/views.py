67f3bfeceb921efee14fbeae04c85865a8e7d2d6
Envoudige <56773097+mentols@users.noreply.github.com>
2022-04-13 21:36:51 +0300

    add auto timezone

--- main/views.py
+++ main/views.py
@@ -1,17 +1,23 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from django.shortcuts import render
 from django.template import RequestContext
 from main.models import Date
-from main.tasks import *
+from main.tasks import adding_task
+
+
+def correct_timezone(local_time):
+    utc_time = datetime.utcnow()
+    correct = local_time - utc_time
+    # print('Correct: ', round(correct.seconds/60/60))
+    return round(correct.seconds/60/60)
 
 
 def index(request):
     if request.method == "POST":
-        date_time = request.POST['date_time']
+        date_time = request.POST['date_time'] + ':00'
         new = Date(date_time=date_time)
         new.save()
-        eta = date_time[0:10] + ' ' + date_time[11:] + ':00'
-        date_time_obj = datetime.strptime(eta, '%Y-%m-%d %H:%M:%S')
-        date_time_obj -= timedelta(hours=3)
-        adding_task.apply_async(args=(eta,), eta=date_time_obj)
+        date_time_obj = datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
+        date_time_obj -= timedelta(hours=correct_timezone(date_time_obj))
+        adding_task.apply_async(args=(date_time,), eta=date_time_obj)
     return render(request, 'main/index.html')
